c017a142b8dd1a92ec3efd58535ae6f91e8c48aa
LeighWeston86 <lweston2107@gmail.com>
2019-04-02 15:20:37 -0700

    clean up

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='sequence_tagging',
+    version='0.0.0',
+    url='https://github.com/LeighWeston86/sequence_tagging',
+    include_package_data=True,
+    packages=find_packages()
+)
